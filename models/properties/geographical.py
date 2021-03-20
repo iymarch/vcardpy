ef2e0429614deaf01a9bff50b6e0d467ef1be7d3
iymarch <iymarch.py@gmail.com>
2021-03-20 22:41:43 +0300

    add geographical properties

---
+++ models/properties/geographical.py
@@ -0,0 +1,11 @@
+from typing import Optional
+from datetime import datetime
+from pydantic import BaseModel, EmailStr, AnyUrl
+
+
+class TZ(BaseModel):
+    pass
+
+
+class GEO(BaseModel):
+    pass
