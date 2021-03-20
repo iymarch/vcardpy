96d48fc4cfe9d60a5b7e6c9d01048575de4f445b
iymarch <iymarch.py@gmail.com>
2021-03-20 22:44:12 +0300

    add parameters models

---
+++ models/parameters/parameters.py
@@ -0,0 +1,48 @@
+from typing import Optional
+from datetime import datetime
+from pydantic import BaseModel, EmailStr, AnyUrl
+
+
+class Language(BaseModel):
+    pass
+
+
+class Value(BasModel):
+    pass
+
+
+class Pref(BaseModel):
+    pass
+
+
+class Altid(BaseModel):
+    pass
+
+
+class PID(BaseModel):
+    pass
+
+
+class Type(BaseModel):
+    pass
+
+
+class Mediatype(BaseModel):
+    pass
+
+
+class Calscale(BaseModel):
+    pass
+
+
+class SortAs(BaseModel):
+    pass
+
+
+class Geo(BaseModel):
+    pass
+
+
+class TZ(BaseModel):
+    pass
+
