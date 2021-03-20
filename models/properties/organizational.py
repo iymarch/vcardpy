1e5ff489e9dde685573b85aceafe92fd5d7d8bf7
iymarch <iymarch.py@gmail.com>
2021-03-20 22:41:01 +0300

    add organizational properties

---
+++ models/properties/organizational.py
@@ -0,0 +1,27 @@
+from typing import Optional
+from datetime import datetime
+from pydantic import BaseModel, EmailStr, AnyUrl
+
+
+class Title(BaseModel):
+    pass
+
+
+class Role(BaseModel):
+    pass
+
+
+class Logo(BaseModel):
+    pass
+
+
+class Org(BaseModel):
+    pass
+
+
+class Member(BaseModel):
+    pass
+
+
+class Related(BaseModel):
+    pass
