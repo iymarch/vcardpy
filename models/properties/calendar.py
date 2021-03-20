3040297f530cc5ce01b95dc8e0bd7cbd93fe6dfe
iymarch <iymarch.py@gmail.com>
2021-03-20 22:43:24 +0300

    add calendar properties

---
+++ models/properties/calendar.py
@@ -0,0 +1,19 @@
+from typing import Optional
+from datetime import datetime
+from pydantic import BaseModel, EmailStr, AnyUrl
+
+
+class FBURL(BaseModel):
+    pass
+
+
+class Caladuri(BaseModel):
+    pass
+
+
+class Caluri(BaseModel):
+    pass
+
+
+class Caluri(BaseModel):
+    pass
