7adf3fe4feff256d00d82290a647c052588f98e7
iymarch <iymarch.py@gmail.com>
2021-03-20 22:43:08 +0300

    add communications properties

---
+++ models/properties/communications.py
@@ -0,0 +1,19 @@
+from typing import Optional
+from datetime import datetime
+from pydantic import BaseModel, EmailStr, AnyUrl
+
+
+class Tel(BaseModel):
+    pass
+
+
+class Email(BaseModel):
+    pass
+
+
+class IMPP(BaseModel):
+    pass
+
+
+class Lang(BaseModel):
+    pass
